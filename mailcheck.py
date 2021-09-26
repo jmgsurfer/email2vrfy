8dc23fd1b5cd47859bdef4b9a89477c5cca5381c
jmgsurfer <gsurfer@free.Fr>
2021-09-26 23:07:40 +0200

    mise à jour mxResolver et ajout fonction smtpChat

--- mailcheck.py
+++ mailcheck.py
@@ -2,7 +2,9 @@ import re
 import argparse
 import whois # modulepython-whois
 import dns.resolver# DNSPYTHON
-
+import socket
+import smtplib
+#
 # [x] parse arguments
 # [x] check email format
 # [x] check if domain name is valid
@@ -34,9 +36,32 @@ def isDomainValid(domain):
         return False
 
 def mxResolver(domain):
+    records = dns.resolver.resolve(domain, 'MX')
+    mxRecord = records[0].exchange
     for x in dns.resolver.resolve(domain, 'MX'):
         print(x.to_text())
+    return str(mxRecord)
 
+def smtpChat(): # source: https://www.scottbrady91.com/Email-Verification/Python-Email-Verification-Script
+    # Get local server hostname
+    host = socket.gethostname()
+
+    # SMTP lib setup (use debug level for full output)
+    server = smtplib.SMTP()
+    server.set_debuglevel(0)
+
+    # SMTP Conversation
+    server.connect(mxRecord)
+    server.helo(host)
+    server.mail('me@domain.com')
+    code, message = server.rcpt(str(addressToVerify))
+    server.quit()
+
+    # Assume 250 as Success
+    if code == 250:
+        print('Y')
+    else:
+        print('N')
 
 args = parse_args()
 mail = args.email
@@ -48,4 +73,4 @@ print(getDomain(mail))
 dom = "Validité domain: " + str(isDomainValid(getDomain(mail)))
 print(dom)
 print("MX extraction")
-mxResolver(getDomain(mail))
+print(mxResolver(getDomain(mail)))
